3b8ec3afb29b422580c1c61bf8d2921f9361ce38
Nitin Ongole <nitinsongole@gmail.com>
2022-03-06 19:22:08 -0800

    added reciprocal velocity obstacle collision avoidance, fixed remove neighbor bug. seems to still be an issue with target assignment or driving.

--- code.py
+++ code.py
@@ -13,13 +13,13 @@ import matplotlib
 args = {"size": 20}
 matplotlib.rc("font", **args)
 
-t = 0.1
+t = 0.01
 class State(Enum):
     IDLE = 0
     DRIVING = 1
     CONSENSUS = 2
-    
-    
+
+
 class Shape:
     def __init__(self,vertices):
         m,d = vertices.shape
@@ -40,14 +40,16 @@ class Drone:
         self.state = State.IDLE
         self.converged = False
         self.cons_neighbors = set()
-        
+        self.vx = 0
+        self.vy = 0
+
     def set_target(self, x, y):
         self.target_x = x
         self.target_y = y
 
     def set_pattern(self,pattern):
         self.pattern = pattern
-    
+
     def neighbor_check(self):
         if len(self.identifier) == 0:
             return True
@@ -61,40 +63,45 @@ class Drone:
                     self.identifier = max_id
                 return True
         return False
-        
-    def get_control(self):
+
+    def set_vel(self):
         if self.neighbor_check():
             self.consensus_step()
-            return 0, 0
+            self.vx = 0
+            self.vy = 0
+        elif np.sqrt((self.x-self.target_x)**2 + (self.y-self.target_y)**2) < 10^-3:
+            self.vx = 0
+            self.vy = 0
         else:
-            u_x = self.target_x - self.x
-            u_y = self.target_y - self.y
-            return u_x, u_y
-    
+            self.set_vel_collision_avoidance();
+
+    def get_vel(self):
+        return self.vx, self.vy
+
     def update_position(self):
-        u_x, u_y = self.get_control()
-        self.x = self.x + u_x*t
-        self.y = self.y + u_y*t
+        self.set_vel()
+        self.x = self.x + self.vx*t
+        self.y = self.y + self.vy*t
         self.log_now()
-    
+
     def log_now(self):
         self.log.append((self.x, self.y))
-        
+
     def get_position(self):
         return np.array([self.x, self.y])
-    
+
     def __repr__(self):
         return "Drone: " + str(self.name) + ", " + str(self.state)
-    
+
     def add_neighbor(self, drone2):
         self.neighbors.add(drone2)
         pass
-    
+
     def remove_neighbor(self, drone2):
         if drone2 in self.neighbors:
-            self.neighbors.remove(d2)    
+            self.neighbors.remove(drone2)
         pass
-    
+
     def consensus_step(self):
         # Check for neighbors with the same ID
         for d in self.neighbors:
@@ -153,6 +160,57 @@ class Drone:
 
         self.set_target(np.mean(inpts[:,0]),np.mean(inpts[:,1]))
 
+    def set_vel_collision_avoidance(self):
+
+        # define some constants defining robot geometries and check_constraints
+        R = 0.1 # radius of circular robots
+        speed_max = 1.5
+        speed_pref = 1
+        speed_min = 0.5
+
+        vA = np.array([[self.vx], [self.vy]])
+        pos_A_target = np.array([[self.target_x-self.x], [self.target_y-self.y]])
+        v_pref = speed_pref * pos_A_target / np.linalg.norm(pos_A_target) # preferred speed in the direction of target
+
+        speeds = np.linspace(speed_min, speed_max, 10)
+        dirs = np.linspace(0., 2*np.pi, 36)
+        vels = []
+        for i in range(10):
+            for j in range(36):
+                vels.append(np.array([[speeds[i]*np.cos(dirs[j])], [speeds[i]*np.sin(dirs[j])]]))
+
+        # Loop through all drones, remove the velocities that are in the velocity obstacles
+
+        for drone in self.neighbors:
+
+            vB = np.array([[drone.vx], [drone.vy]])
+            pos_AB = np.array([[drone.x-self.x], [drone.y-self.y]])
+            dist_AB = np.linalg.norm(pos_AB)
+            theta = np.arcsin(2.2*R / dist_AB)
+
+            a_right = np.array([[np.cos(np.pi/2-theta), np.sin(np.pi/2-theta)], [-np.sin(np.pi/2-theta), np.cos(np.pi/2-theta)]]) @ pos_AB/dist_AB
+            a_left = np.array([[np.cos(theta-np.pi/2), np.sin(theta-np.pi/2)], [-np.sin(theta-np.pi/2), np.cos(theta-np.pi/2)]]) @ pos_AB/dist_AB
+
+            counter = 0
+            while counter < len(vels):
+                if a_right.T @ (2*vels[counter] - vA - vB) > 0 and a_left.T @ (2*vels[counter] - vA - vB) > 0:
+                    vels.pop(counter)
+                else:
+                    counter = counter + 1
+
+        # Optimize over the velocities that are left
+
+        if len(vels) == 0:
+            self.vx = 0
+            self.vy = 0
+        else:
+            vels_possible = np.array(vels).reshape((len(vels), 2)).T
+            cost = np.linalg.norm(vels_possible-v_pref, axis=0)
+            best_index = np.argmin(cost)
+
+            self.vx = vels_possible[0,best_index]
+            self.vy = vels_possible[1,best_index]
+
 
 class GhostServer:
     def __init__(self, drones, targets, max_dist = 5):
@@ -160,19 +218,19 @@ class GhostServer:
         self.max_dist = max_dist
         self.targets = targets
         pass
-    
+
     def update_pattern(self,pattern):
         for d in self.fleet:
             d.set_pattern(pattern)
-    
+
     def update_targets(self, targets=None):
         for d1 in self.fleet:
             d1.set_target(*self.targets[d1.name])
-    
+
     def neighbors(self, pos_1, pos_2):
         if np.linalg.norm(pos_1 - pos_2) < self.max_dist:
             return True
-    
+
     def update_neighbors(self):
         for d1 in self.fleet:
             fleet_copy = self.fleet.copy()
@@ -182,45 +240,45 @@ class GhostServer:
                     d1.add_neighbor(d2)
                 else:
                     d1.remove_neighbor(d2)
-                    
+
     def update_positions(self):
         for d1 in self.fleet:
             d1.update_position()
-    
-    def return_trajectories(self):        
+
+    def return_trajectories(self):
         return np.array([d.log for d in self.fleet])
-        
-    
-    
+
+
+
 if __name__ == "__main__":
-    #%% Initialization 
-    d1 = Drone(-1, 0, 0, "LU") #identifier likely initialized as empty string ""   
+    #%% Initialization
+    d1 = Drone(-1, 0, 0, "LU") #identifier likely initialized as empty string ""
     d2 = Drone(1, 0, 1, "LRU")
     d3 = Drone(1, 1, 2, "LUD")
     d4 = Drone(-1, 1, 3, "LRD")
-    
-    fleet = [d1, d2, d3, d4] 
+
+    fleet = [d1, d2, d3, d4]
     targets = {0: (0, 1), 1: (0, 1), 2: (0, 1), 3: (0, 1)} #ideally targets shouldn't come from here
     cmap = {0:"blue", 1: "red", 2: "green", 3: "black"} #for plotting
-    
+
     GS = GhostServer(fleet, targets, max_dist=1.0) #comm. range = max_dist
-    
+
     #some stuff to establish the neighborset and give some dummy targets
     GS.update_neighbors()
     GS.update_targets()
-    
-    
-    #%% Simulation loop       
+
+
+    #%% Simulation loop
     for i in range(20):
         GS.update_neighbors()
         GS.update_positions()
-    
+
     trajectories = GS.return_trajectories()
-    #%% Plotting     
-    
+    #%% Plotting
+
     # for i in range(len(fleet)):
     #     plt.plot(trajectories[i, :, 0], trajectories[i, :, 1], label="Drone: "+str(i))
-    
+
     fig, ax = plt.subplots(figsize=[10, 10])
     ax.set_xlim(-2, 2)
     ax.set_ylim(0, 2)
@@ -231,25 +289,24 @@ if __name__ == "__main__":
         plt.plot(trajectories[i, 0, 0], trajectories[i, 0, 1], label="Drone: "+str(i), color = cmap[i])
         plt.xlabel("$x$ position")
         plt.ylabel("$y$ position")
-        
+
     def init():
           # sns.heatmap(data[0], vmax=1, square=True, cbar=False)
         for i in range(len(fleet)):
             plt.plot(trajectories[i, 0, 0], trajectories[i, 0, 1], label="Drone: "+str(i), color = cmap[i])
         plt.xlabel("$x$ position")
         plt.ylabel("$y$ position")
-    
+
     def animate(j):
         # sns.heatmap(data[i], vmax=1, square=True, cbar=False)
         for i in range(len(fleet)):
             plt.plot(trajectories[i, :j, 0], trajectories[i, :j, 1], label="Drone: "+str(i), color = cmap[i])
         plt.xlabel("$x$ position")
         plt.ylabel("$y$ position")
-    
+
     anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(trajectories[0]), repeat = True)
-    
+
     #%% save animation
-    f = r"trajectories2.mp4" 
-    writervideo = animation.FFMpegWriter(fps=1/t) 
+    f = r"trajectories2.mp4"
+    writervideo = animation.FFMpegWriter(fps=1/t)
     anim.save(f, writer=writervideo)
-    
